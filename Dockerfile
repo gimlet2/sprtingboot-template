e0fb0122937681fae67677f0e3079e6d2ea4e203
gimlet2 <andrey.chernishov@gmail.com>
2019-02-12 23:45:38 +0100

    Template springboot project

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM debian:9-slim AS builder
+
+RUN set -ex && \
+    apt-get update && apt-get install -y wget unzip && \
+    wget https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz -nv -O jdk.tar.gz && \
+    mkdir -p /opt/jdk && \
+    tar zxvf jdk.tar.gz -C /opt/jdk --strip-components=1 && \
+    rm jdk.tar.gz && \
+    rm /opt/jdk/lib/src.zip
+
+RUN /opt/jdk/bin/jlink \
+    --module-path /opt/jdk/jmods \
+    --verbose \
+    --add-modules java.base,java.logging,java.xml,jdk.unsupported,java.sql,java.naming,java.desktop,java.management,java.security.jgss,java.instrument \
+    --output /opt/jdk-minimal \
+    --compress 2 \
+    --no-header-files
+
+# Second stage, add only our custom jdk9 distro and our app
+FROM debian:9-slim
+COPY --from=builder /opt/jdk-minimal /opt/jdk-minimal
+
+ENV JAVA_HOME=/opt/jdk-minimal
+ENV PATH="$PATH:$JAVA_HOME/bin"
+
+EXPOSE 8080
+COPY build/libs/app.jar /app.jar
+ENTRYPOINT ["sh", "-c", "java $JAVA_OPTS -jar /app.jar"]
---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+	id("io.spring.dependency-management") version "1.0.6.RELEASE"
+	id("org.jetbrains.kotlin.jvm") version "1.3.21"
+	id("org.jetbrains.kotlin.plugin.spring") version "1.3.21"
+	id("org.springframework.boot") version "2.1.2.RELEASE"
+	id("com.gorylenko.gradle-git-properties") version "2.0.0"
+}
+
+
+group = "com.restmonkeys"
+version = "0.0.1-SNAPSHOT"
+val bootJar: BootJar by tasks
+bootJar.apply {
+	archiveName = "app.jar"
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+springBoot {
+	buildInfo()
+}
+
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("ch.qos.logback:logback-classic:1.2.3")
+	implementation("ch.qos.logback:logback-core:1.2.3")
+	implementation("net.logstash.logback:logstash-logback-encoder:5.2")
+	implementation("io.micrometer:micrometer-registry-prometheus:1.1.1")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		languageVersion = "1.3"
+		jvmTarget = "1.8"
+	}
+}
