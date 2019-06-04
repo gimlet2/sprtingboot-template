c1e80245656c7e180a27c308c01df8014b66f936
Andrey <andrey.chernishov@gmail.com>
2019-06-04 17:27:29 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
 	id("io.spring.dependency-management") version "1.0.7.RELEASE"
-	id("org.jetbrains.kotlin.jvm") version "1.3.30"
-	id("org.jetbrains.kotlin.plugin.spring") version "1.3.30"
-	id("org.springframework.boot") version "2.1.4.RELEASE"
+	id("org.jetbrains.kotlin.jvm") version "1.3.31"
+	id("org.jetbrains.kotlin.plugin.spring") version "1.3.31"
+	id("org.springframework.boot") version "2.1.5.RELEASE"
 	id("com.gorylenko.gradle-git-properties") version "2.0.0"
 }
 
@@ -34,7 +34,7 @@ repositories {
 dependencies {
 	implementation("ch.qos.logback:logback-classic:1.2.3")
 	implementation("ch.qos.logback:logback-core:1.2.3")
-	implementation("net.logstash.logback:logstash-logback-encoder:5.3")
+	implementation("net.logstash.logback:logstash-logback-encoder:6.0")
 	implementation("io.micrometer:micrometer-registry-prometheus:1.1.4")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
